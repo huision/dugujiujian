ffa3b7a07bb244c19ec26a3c665f024e9c02d2d9
zh <zh@163.com>
2018-12-05 19:28:47 +0800

    修改2

--- dugujiujian/src/main/java/com/atguigu/Test.java
+++ dugujiujian/src/main/java/com/atguigu/Test.java
@@ -2,9 +2,11 @@ package com.atguigu;
 
 public class Test {
     public static void main(String[] args) {
+
         System.out.println("111111111111111111111");
         System.out.println("2222222222222222222222");
         System.out.println("33333333333333333333");
+        System.out.println("4444444444444444444444");
         System.out.println("4444444444444444444444");
     }
 }
